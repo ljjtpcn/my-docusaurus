fde0483e843993bf2e448200d3c8e43e94105ba2
ljjtpcn <ljjtpcn@163.com>
2022-05-01 09:18:20 +0800

    init

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,113 +1,288 @@
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-
-/** @type {import('@docusaurus/types').DocusaurusConfig} */
-module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-  i18n: {
-    defaultLocale: "zh-Hans",
-    locales: ["zh-Hans"]
+// @ts-check
+const path = require('path')
+const beian = '湘 ICP 备 2021016514 号'
+const friendLinks = [
+  {
+    label: '峰华前端工程师',
+    to: 'https://zxuqian.cn/',
   },
+]
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'TwoPair',
+  titleDelimiter: '-',
+  url: 'https://blog.twopair.cn',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'ljjtpcn', // Usually your GitHub org/user name.
+  projectName: 'ljjtpcn', // Usually your repo name.
+  /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
   themeConfig: {
+    image: 'img/TwoPair.jpg',
+    announcementBar: {
+      id: 'announcementBar-2', // Any value that will identify this message.
+      content: `代码能重写，人不能重来`,
+    },
+    metadata: [
+      {
+        name: 'keywords',
+        content: 'TwoPair, TwoPair, blog, javascript, typescript, python ,node, react, vue, web, 前端, 后端',
+      },
+    ],
+    hideableSidebar: true,
     navbar: {
-      title: 'My Site',
+      title: 'TwoPair',
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: 'TwoPair',
+        src: 'img/logo.webp',
+        srcDark: 'img/logo.webp',
       },
       items: [
         {
-          type: 'doc',
-          docId: 'intro',
-          position: 'left',
-          label: 'Tutorial',
-        },
-        {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
+          label: '标签',
+          to: 'tags',
           position: 'right',
+        },
+        {
+          label: '归档',
+          to: 'archive',
+          position: 'right',
+        },
+        {
+          label: '学习',
+          position: 'right',
+          items: [
+            {
+              label: '技术笔记',
+              to: 'docs/skill/',
+            },
+            // {
+            //   label: "个人推荐",
+            //   to: "docs/recommend",
+            // },
+          ],
+        },
+        // {
+        //   label: '小工具',
+        //   position: 'right',
+        //   items: [
+        //     {
+        //       label: '资源导航',
+        //       to: '/resources', // 'https://nav.TwoPair.cn'
+        //     },
+        //     {
+        //       label: 'JS代码混淆与还原',
+        //       to: 'https://deobfuscator.TwoPair.cn', // 'https://deobfuscator.TwoPair.cn'
+        //     },
+        //     {
+        //       label: 'CyberChef在线加解密',
+        //       to: 'http://cipher.TwoPair.cn', // 'http://cipher.TwoPair.cn'
+        //     },
+        //     {
+        //       label: 'TwoPair在线工具',
+        //       to: 'http://tools.TwoPair.cn', // 'http://tools.TwoPair.cn'
+        //     },
+        //   ],
+        // },
+        {
+          label: '实战项目',
+          position: 'right',
+          to: '/project',
         },
       ],
     },
+    // algolia: {
+    //   apiKey: '87223cb5a5ff37c4dbbb616812c65a59',
+    //   appId: '2NBW5YNFON',
+    //   indexName: 'TwoPair',
+    // },
     footer: {
       style: 'dark',
       links: [
         {
-          title: 'Docs',
+          title: '学习',
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: '技术博客',
+              to: '/#homepage_blogs',
+            },
+            {
+              label: '技术笔记',
+              to: 'docs/skill',
+            },
+            {
+              label: '实战项目',
+              to: 'project',
             },
           ],
         },
         {
-          title: 'Community',
+          title: '社交媒体',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              label: '首页',
+              to: '/',
             },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
+            // {
+            //   label: '关于我',
+            //   to: '/about',
+            // },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/ljjtpcn',
             },
+            // {
+            //   label: '掘金',
+            //   href: 'https://juejin.cn/user/1565318510545901',
+            // },
           ],
         },
+        {
+          title: '友情链接',
+          items: friendLinks,
+        },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `<p>Copyright © ${new Date().getFullYear()} TwoPair Built with Docusaurus.</p><p><a href="http://beian.miit.gov.cn/" >${beian}</a>`,
+      //</p><a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Creative Commons License" style="border-width:0" src="/img/creative-commons-license-icon.png" /></a><br />本站所有内容遵循 <a rel="license" href="https://creativecommons.org/licenses/by-nc/4.0/deed.zh-Hans" >CC BY-NC 4.0 协议</a>，转载须注明署名和出处，且不可用于商业用途。若与其他同步平台协议冲突，以本网站为准。
     },
     prism: {
-      theme: lightCodeTheme,
-      darkTheme: darkCodeTheme,
+      theme: require('prism-react-renderer/themes/github'),
+      darkTheme: require('prism-react-renderer/themes/vsDark'),
+      additionalLanguages: ['java', 'php'],
+      // defaultLanguage: "javascript",
     },
+    tableOfContents: {
+      minHeadingLevel: 2,
+      maxHeadingLevel: 4,
+    },
+    zoomSelector: '.markdown :not(em) > img',
+    liveCodeBlock: {
+      playgroundPosition: 'top',
+    },
+    // googleAnalytics: {
+    //   trackingID: "UA-118572241-1",
+    //   anonymizeIP: true, // Should IPs be anonymized?
+    // },
+    // gtag: {
+    //   trackingID: "G-6PSESJX0BM",
+    //   // Optional fields.
+    //   anonymizeIP: true, // Should IPs be anonymized?
+    // },
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
-      {
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+          path: 'docs',
+          sidebarPath: 'sidebars.js',
+          // editUrl: "https://github.com/TwoPair/TwoPair.cn/tree/master",
+          // remarkPlugins: [require("remark-math")],
+          // rehypePlugins: [require("rehype-katex")],
+          // showLastUpdateAuthor: true,
+          // showLastUpdateTime: true,
         },
         blog: {
+          path: 'blog',
+          routeBasePath: '/',
+          blogSidebarTitle: '近期文章',
+          blogSidebarCount: 5,
+          postsPerPage: 10,
+          // remarkPlugins: [require("remark-math")],
+          // rehypePlugins: [require("rehype-katex")],
           showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+          readingTime: ({ content, frontMatter, defaultReadingTime }) =>
+            defaultReadingTime({ content, options: { wordsPerMinute: 300 } }),
+          feedOptions: {
+            type: 'all',
+            title: 'TwoPair',
+            copyright: `Copyright © ${new Date().getFullYear()} TwoPair Built with Docusaurus.<p><a href="http://beian.miit.gov.cn/" class="footer_lin">${beian}</a></p>`,
+          },
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: [require.resolve('./src/css/custom.css')],
         },
-      },
+        sitemap: {
+          changefreq: 'daily',
+          priority: 0.5,
+        },
+        // debug: true,
+      }),
     ],
   ],
-};
+  themes: ['@docusaurus/theme-live-codeblock'],
+  plugins: [
+    path.resolve(__dirname, './src/plugin/plugin-baidu-analytics'),
+    path.resolve(__dirname, './src/plugin/plugin-baidu-push'),
+    // path.resolve(__dirname, "./src/plugin/plugin-onesignal-push"),
+    // "docusaurus2-dotenv",
+    '@docusaurus/plugin-ideal-image',
+    path.resolve(__dirname, './src/plugin/plugin-image-zoom'),
+    path.resolve(__dirname, './src/plugin/plugin-latest-docs'),
+    // [
+    //   "@easyops-cn/docusaurus-search-local",
+    //   {
+    //     hashed: true,
+    //     // indexPages: true,
+    //     blogRouteBasePath: "/",
+    //     language: ["en", "zh"],
+    //   },
+    // ],
+    // [
+    //   '@docusaurus/plugin-pwa',
+    //   {
+    //     debug: true,
+    //     offlineModeActivationStrategies: ['appInstalled', 'standalone', 'queryString'],
+    //     pwaHead: [
+    //       {
+    //         tagName: 'link',
+    //         rel: 'icon',
+    //         href: '/img/TwoPair.jpg',
+    //       },
+    //       {
+    //         tagName: 'link',
+    //         rel: 'manifest',
+    //         href: '/manifest.json', // 您的 PWA Manifest
+    //       },
+    //       {
+    //         tagName: 'meta',
+    //         name: 'theme-color',
+    //         content: 'rgb(51 139 255)',
+    //       },
+    //     ],
+    //   },
+    // ],
+  ],
+  stylesheets: [
+    // {
+    //   rel: "preconnect",
+    //   href: "https://fonts.gstatic.com",
+    //   type: "text/css",
+    // },
+    /* {
+      href: "/katex/katex.min.css",
+      type: "text/css",
+      integrity:
+        "sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X",
+      crossorigin: "anonymous",
+    }, */
+    // {
+    //   href: "https://fonts.font.im/css?family=Raleway:500,700&display=swap",
+    //   type: "text/css",
+    //   rel: "stylesheet",
+    // },
+    // {
+    //   href: "https://fonts.googleapis.com/css2?family=Fira+Code&display=swap",
+    //   type: "text/css",
+    //   rel: "stylesheet",
+    // },
+  ],
+  i18n: {
+    defaultLocale: 'zh',
+    locales: ['zh'],
+  },
+  onBrokenLinks: 'ignore',
+}
+
+module.exports = config
---
+++ src/data/project.tsx
@@ -0,0 +1,119 @@
+import { sortBy } from '@site/src/utils/jsUtils'
+
+export type Tag = {
+  label: string
+  description: string
+  color: string
+}
+
+export type TagType = 'favorite' | 'opensource' | 'product' | 'design' | 'javascript' | 'typescript' | 'nodejs'
+
+export type Project = {
+  title: string
+  description: string
+  preview?: any
+  website: string
+  source?: string | null
+  tags: TagType[]
+}
+
+export const Tags: Record<TagType, Tag> = {
+  favorite: {
+    label: 'Favorite',
+    description: 'Our favorite Docusaurus sites that you must absolutely check-out!',
+    color: '#e9669e',
+  },
+  opensource: {
+    label: '开源',
+    description: 'Open-Source Docusaurus sites can be useful for inspiration!',
+    color: '#39ca30',
+  },
+  product: {
+    label: '产品',
+    description: 'Docusaurus sites associated to a commercial product!',
+    color: '#dfd545',
+  },
+  design: {
+    label: '设计',
+    description: 'Beautiful Docusaurus sites, polished and standing out from the initial template!',
+    color: '#a44fb7',
+  },
+  javascript: {
+    label: 'JavaScript',
+    description: 'JavaScript project',
+    color: '#dfd545',
+  },
+  typescript: {
+    label: 'TypeScript',
+    description: 'JavaScript project',
+    color: '#007acc',
+  },
+  nodejs: {
+    label: 'NodeJS',
+    description: 'NodeJS project',
+    color: '#39ca30',
+  },
+}
+
+const Projects: Project[] = [
+  {
+    title: '愧怍的小站',
+    description: '基于Docusaurus v2 静态网站生成器实现个人博客',
+    preview: require('./showcase/blog.png'),
+    website: 'https://kuizuo.cn',
+    source: 'https://github.com/kuizuo/blog',
+    tags: ['opensource', 'design'],
+  },
+  {
+    title: '资源导航',
+    description: '学习编程中遇到的资源整合网站',
+    preview: require('./showcase/nav.png'),
+    website: 'https://nav.kuizuo.cn',
+    source: 'https://github.com/kuizuo/code-nav',
+    tags: ['opensource', 'javascript'],
+  },
+  // {
+  //   title: 'kz-admin',
+  //   description: '基于Vue + Vben + NestJs + TypeScript + TypeORM + MySql + Redis编写的一款前后端分离的权限管理系统',
+  //   preview: require('./showcase/blog.png'),
+  //   website: 'https://admin.kuizuo.cn',
+  //   source: 'https://github.com/kuizuo/kz-admin',
+  //   tags: ['opensource', 'typescript'],
+  // },
+  {
+    title: 'JS代码混淆与还原',
+    description: '基于Babel的AST操作对JavaScript代码混淆与还原的网站',
+    preview: require('./showcase/js-de-obfuscator.png'),
+    website: 'https://deobfuscator.kuizuo.cn',
+    source: 'https://github.com/kuizuo/js-de-obfuscator',
+    tags: ['opensource', 'typescript', 'nodejs'],
+  },
+  {
+    title: '愧怍在线工具',
+    description: '基于React与MUI组件库编写的在线工具网站',
+    preview: require('./showcase/tools.png'),
+    website: 'http://tools.kuizuo.cn',
+    source: 'https://github.com/kuizuo/online-tools',
+    tags: ['opensource', 'typescript', 'nodejs'],
+  },
+  {
+    title: '题小侠',
+    description: '基于Taro+Vue3+NutUI编写的搜题小程序',
+    preview: require('./showcase/question-man.png'),
+    website: 'https://img.kuizuo.cn/itopic.jpg',
+    source: 'https://github.com/kuizuo/question-man',
+    tags: ['opensource', 'typescript', 'nodejs'],
+  },
+]
+
+export const TagList = Object.keys(Tags) as TagType[]
+function sortProject() {
+  let result = Projects
+  // Sort by site name
+  // result = sortBy(result, (user) => user.title.toLowerCase());
+  // Sort by favorite tag, favorites first
+  // result = sortBy(result, (user) => !user.tags.includes('javascript'));
+  return result
+}
+
+export const sortedProjects = sortProject()
